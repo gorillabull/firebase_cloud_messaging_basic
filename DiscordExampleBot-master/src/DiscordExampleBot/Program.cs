c34c6883441afad39f4f08a27fe0faf5c095cc2f
gorilla <thisisntlongonlytomakelifehard@gmail.com>
2018-02-13 15:35:18 -0800

    first

---
+++ DiscordExampleBot-master/src/DiscordExampleBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace DiscordExampleBot
+{
+    public class Program
+    {
+        // Convert our sync main to an async main.
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+            // Define the DiscordSocketClient
+            client = new DiscordSocketClient();
+
+            var token = "token here";
+
+            // Login and connect to Discord.
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.ConnectAsync();
+
+            var map = new DependencyMap();
+            map.Add(client);
+
+            handler = new CommandHandler();
+            await handler.Install(map);
+            
+            // Block this program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
